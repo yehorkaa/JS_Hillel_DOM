e4664a62299e5f3e9f63dc31dd99fa3232e6b004
Yehor <107763117+yehorkaa@users.noreply.github.com>
2022-12-14 07:50:59 +0200

    info

---
+++ js/dom.js
@@ -0,0 +1,32 @@
+// Є текстове поле на сторінці. Під час фокусування на цьому полі збоку
+//  з'являється <div>. При пропажі фокусу - <div> так само пропадає
+
+//использовать адд лисенер и вставить элемент
+
+const text = document.querySelector('.text');
+console.log(text)
+document.querySelector('p').style.color = 'red'
+const container = document.getElementById('parent')
+const div = document.createElement('div');
+div.classList.add('black')
+
+text.addEventListener('mouseover', function(event) {
+    
+    container.appendChild(div)
+    
+})
+
+
+text.addEventListener('mouseleave', function(event) {
+    
+    div.remove()
+        
+    })
+
+
+
+
+
+// во втором нужно криэйт элемент, аппенд чайлд
+
+//если первый вложенный массив то 1.1 если второй то 2.1 и тд
